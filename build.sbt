c5ee37c63d82931577aef355d6496b70b3cfe156
Edgar Orendain <edgar@orendainx.com>
2017-01-23 12:32:45 -0800

    Flush changes for integration with larger trucking-iot project

--- build.sbt
+++ build.sbt
@@ -1,6 +1,6 @@
 name := "trucking-topology"
 
-version := "0.3"
+version := "0.3.1"
 
 organization := "com.hortonworks.orendainx"
 
@@ -11,41 +11,53 @@ resolvers += Resolver.mavenLocal
 
 libraryDependencies ++= Seq(
 
-  "com.hortonworks.orendainx" %% "trucking-shared" % "0.3",
+  "com.hortonworks.orendainx" %% "trucking-shared" % "0.3.1",
 
   "org.apache.nifi" % "nifi-storm-spout" % "1.1.0",
 
   ("com.hortonworks.registries" % "schema-registry-serdes" % "0.1.0-SNAPSHOT")
       .exclude("org.slf4j", "log4j-over-slf4j")
-      .exclude("commons-collections", "commons-collections")
-      .exclude("commons-beanutils", "commons-beanutils")
-      .exclude("commons-beanutils-core", "commons-beanutils-core")
-      .exclude("org.glassfish.hk2.external", "aopalliance-repackaged")
-      .exclude("org.glassfish.hk2.external", "javax.inject")
-      .exclude("javax.ws.rs", "javax.ws.rs-api"),
+      //.exclude("commons-collections", "commons-collections")
+      //.exclude("commons-beanutils", "commons-beanutils")
+      .exclude("commons-beanutils", "commons-beanutils-core"),
+      //.exclude("org.glassfish.hk2.external", "aopalliance-repackaged")
+      //.exclude("org.glassfish.hk2.external", "javax.inject"),
+      //.exclude("com.sun.jersey", "jersey-core"),
+      //.exclude("javax.ws.rs", "javax.ws.rs-api"),
+
 
   // HDP will provide it's own Storm Jar, so we mark it as "provided"
   "org.apache.storm" % "storm-core" % "1.0.2" % "provided",
 
-  "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0",
 
 
-  ("org.apache.hadoop" % "hadoop-common" % "2.7.3")
+
+  /*("org.apache.hadoop" % "hadoop-common" % "2.7.3")
     .exclude("commons-collections", "commons-collections")
     .exclude("commons-beanutils", "commons-beanutils")
     .exclude("commons-beanutils", "commons-beanutils-core")
-    .exclude("org.slf4j", "slf4j-log4j12"),
-  ("org.apache.hadoop" % "hadoop-client" % "2.7.3")
-    .exclude("commons-collections", "commons-collections")
-    .exclude("commons-beanutils", "commons-beanutils") // vs below and commons-collections:commons-collections in itself
-    .exclude("commons-beanutils", "commons-beanutils-core") // vs above and commons-collections:commons-collections in itself
-    .exclude("org.slf4j", "slf4j-log4j12"),
+    .exclude("org.slf4j", "slf4j-log4j12"),*/
+  //("org.apache.hadoop" % "hadoop-client" % "2.7.3")
+  //  .exclude("commons-collections", "commons-collections")
+  //  .exclude("commons-beanutils", "commons-beanutils") // vs below and commons-collections:commons-collections in itself
+  //  .exclude("commons-beanutils", "commons-beanutils-core") // vs above and commons-collections:commons-collections in itself
+  //  .exclude("org.slf4j", "slf4j-log4j12"),
   // TODO: Review: Verify above is not included with Storm, and does not just require classpath fix
 
 
   "com.typesafe" % "config" % "1.3.1",
-  "com.github.pathikrit" %% "better-files" % "2.16.0"
+  "com.github.pathikrit" %% "better-files" % "2.16.0",
+
+  // Logging
+  "com.typesafe.scala-logging" %% "scala-logging" % "3.5.0"
 )
+
+assemblyMergeStrategy in assembly := {
+  case PathList("org", "apache", "commons", "collections", xs @ _*)      => MergeStrategy.first
+  case x =>
+    val oldStrategy = (assemblyMergeStrategy in assembly).value
+    oldStrategy(x)
+}
 
 // Export Jars, not class files
 exportJars := true
--- src/main/scala/com/hortonworks/orendainx/trucking/topology/TruckingTopology.scala
+++ src/main/scala/com/hortonworks/orendainx/trucking/topology/TruckingTopology.scala
@@ -1,6 +1,9 @@
 package com.hortonworks.orendainx.trucking.topology
 
-import com.hortonworks.orendainx.trucking.topology.bolts.TruckEventAndTrafficDataMergeBolt
+import better.files.File
+import com.hortonworks.orendainx.trucking.topology.bolts.TruckAndTrafficMergeBolt
+import com.hortonworks.orendainx.trucking.topology.nifi.MergedDataPacketBuilder
+import com.hortonworks.registries.schemaregistry.client.SchemaRegistryClient
 import com.typesafe.config.{ConfigFactory, Config => TypeConfig}
 import com.typesafe.scalalogging.Logger
 import org.apache.nifi.remote.client.SiteToSiteClient
@@ -8,48 +11,40 @@ import org.apache.nifi.storm.{NiFiBolt, NiFiSpout}
 import org.apache.storm.generated.StormTopology
 import org.apache.storm.topology.TopologyBuilder
 import org.apache.storm.topology.base.BaseWindowedBolt
-import org.apache.storm.tuple.Fields
 import org.apache.storm.{Config, StormSubmitter}
 
 import scala.concurrent.duration._
 
 /**
   * Companion object to [[TruckingTopology]] class.
-  * Provides an entry point for passing configuration changes.
+  * Provides an entry point for passing in a custom configuration file.
   *
   * @author Edgar Orendain <edgar@orendainx.com>
   */
 object TruckingTopology {
 
   // NiFi constants
-  val NiFiUrl = "nifi.url"
-  val NiFiOutputPortName = "nifi.output.port-name"
-  val NiFiOutputBatchSize = "nifi.output.batch-size"
-
-  val NiFiInputPortName = "nifi.input.port-name"
-  val NiFiInputBatchSize = "nifi.input.batch-size"
-  val NiFiInputTickFrequency = "nifi.input.tick-frequency"
+  private val NiFiUrl = "nifi.url"
+  private val NiFiInputPortName = "nifi.input.port-name"
+  private val NiFiInputBatchSize = "nifi.input.batch-size"
+  private val NiFiInputTickFrequency = "nifi.input.tick-frequency"
 
   def main(args: Array[String]): Unit = {
 
-    // Extract specified configuration file path from supplied argument, else throw Exception
-    val configName = if (args.nonEmpty) args(0) else throw new IllegalArgumentException("Must specify name of configuration file.")
-    val config = ConfigFactory.load(configName)
+    // Either read in a path to a config file, or use the default one
+    val config = if (args.nonEmpty) ConfigFactory.parseFile(File(args(0)).toJava) else ConfigFactory.load()
 
     // Set up configuration for the Storm Topology
     val stormConfig = new Config()
     stormConfig.setDebug(config.getBoolean(Config.TOPOLOGY_DEBUG))
     stormConfig.setMessageTimeoutSecs(config.getInt(Config.TOPOLOGY_MESSAGE_TIMEOUT_SECS))
     stormConfig.setNumWorkers(config.getInt(Config.TOPOLOGY_WORKERS))
-
+    stormConfig.put(SchemaRegistryClient.Configuration.SCHEMA_REGISTRY_URL.name(), config.getString("schema-registry.url"))
+    stormConfig.put("emptyConfig", new java.util.HashMap[String, String])
     // TODO: would be nice if storm.Config had "setProperty" to hide hashmap implementation
-    stormConfig.put("emptyConfig", new java.util.HashMap[String, String]) // Because storm -_-
 
-    // Build out a TruckTopology
-    val topology = new TruckingTopology(config).buildTopology()
-
-    // Submit the topology to run on the cluster
-    StormSubmitter.submitTopology("truckTopology", stormConfig, topology)
+    // Build and submit the topology
+    StormSubmitter.submitTopology("truckingTopology", stormConfig, new TruckingTopology(config).buildTopology())
   }
 }
 
@@ -57,33 +52,33 @@ object TruckingTopology {
   * Create a topology with the following components.
   *
   * Spouts:
-  *   - NiFiSpout (for injesting trucking events from NiFi)
+  *   - NiFiSpout (for injesting TruckData from NiFi)
+  *   - NiFiSpout (for injesting TrafficData from NiFi)
   * Bolt:
-  *   - RouterBolt
-  *   - NiFiBolt (for sending back out to NiFi)
+  *   - TruckAndTrafficMergeBolt (for merging TruckData and TrafficData streams into one)
+  *   - NiFiBolt (for sending merged data back out to NiFi)
   *
   * @author Edgar Orendain <edgar@orendainx.com>
   */
 class TruckingTopology(config: TypeConfig) {
 
-  lazy val logger = Logger(classOf[TruckingTopology])
+  private lazy val logger = Logger(classOf[TruckingTopology])
 
   /**
     *
     * @return a built StormTopology
     */
   def buildTopology(): StormTopology = {
+
     // Builder to perform the construction of the topology.
     implicit val builder = new TopologyBuilder()
 
     // Build Nifi Spouts to ingest trucking data
-    buildNifiTruckEventSpout()
+    buildNifiTruckDataSpout()
     buildNifiTrafficDataSpout()
 
-    // Build Bolt to merge data streams together with windowing
+    // Build Bolt to merge data streams together with windowing and another to push back out to NiFi
     buildMergeBolt()
-
-    // Build Nifi Bolt for pushing values back to Nifi
     buildNifiBolt()
 
     logger.info("Storm topology finished building.")
@@ -92,42 +87,32 @@ class TruckingTopology(config: TypeConfig) {
     builder.createTopology()
   }
 
-  def buildNifiTruckEventSpout()(implicit builder: TopologyBuilder): Unit = {
+  def buildNifiTruckDataSpout()(implicit builder: TopologyBuilder): Unit = {
     // Extract values from config
     val nifiUrl = config.getString(TruckingTopology.NiFiUrl)
-    val nifiPortName = config.getString("truck-events.port-name")
-    val batchSize = config.getInt("truck-events.batch-size")
+    val nifiPortName = config.getString("nifi.truck-data.port-name")
+    val batchSize = config.getInt("nifi.truck-data.batch-size")
     val taskCount = config.getInt(Config.TOPOLOGY_TASKS)
 
     // This assumes that the data is text data, as it will map the byte array received from NiFi to a UTF-8 Encoded string.
-    val client = new SiteToSiteClient.Builder()
-      .url(nifiUrl)
-      .portName(nifiPortName)
-      .requestBatchCount(batchSize)
-      .buildConfig()
+    val client = new SiteToSiteClient.Builder().url(nifiUrl).portName(nifiPortName).requestBatchCount(batchSize).buildConfig()
 
     // Create a spout with the specified configuration, and place it in the topology blueprint
-    val nifiSpout = new NiFiSpout(client)
-    builder.setSpout("truckEvents", nifiSpout, taskCount)
+    builder.setSpout("truckData", new NiFiSpout(client), taskCount)
   }
 
   def buildNifiTrafficDataSpout()(implicit builder: TopologyBuilder): Unit = {
     // Extract values from config
     val nifiUrl = config.getString(TruckingTopology.NiFiUrl)
-    val nifiPortName = config.getString("traffic-data.port-name")
-    val batchSize = config.getInt("traffic-data.batch-size")
+    val nifiPortName = config.getString("nifi.traffic-data.port-name")
+    val batchSize = config.getInt("nifi.traffic-data.batch-size")
     val taskCount = config.getInt(Config.TOPOLOGY_TASKS)
 
     // This assumes that the data is text data, as it will map the byte array received from NiFi to a UTF-8 Encoded string.
-    val client = new SiteToSiteClient.Builder()
-      .url(nifiUrl)
-      .portName(nifiPortName)
-      .requestBatchCount(batchSize)
-      .buildConfig()
+    val client = new SiteToSiteClient.Builder().url(nifiUrl).portName(nifiPortName).requestBatchCount(batchSize).buildConfig()
 
     // Create a spout with the specified configuration, and place it in the topology blueprint
-    val nifiSpout = new NiFiSpout(client)
-    builder.setSpout("trafficData", nifiSpout, taskCount)
+    builder.setSpout("trafficData", new NiFiSpout(client), taskCount)
   }
 
   def buildMergeBolt()(implicit builder: TopologyBuilder): Unit = {
@@ -136,13 +121,10 @@ class TruckingTopology(config: TypeConfig) {
     val duration = config.getInt(Config.TOPOLOGY_BOLTS_WINDOW_LENGTH_DURATION_MS)
 
     // Create a bolt with a tumbling window
-    val windowDuration = new BaseWindowedBolt.Duration(duration, MILLISECONDS)
-    val bolt = new TruckEventAndTrafficDataMergeBolt().withTumblingWindow(windowDuration)
+    val bolt = new TruckAndTrafficMergeBolt().withTumblingWindow(new BaseWindowedBolt.Duration(duration, MILLISECONDS))
 
     // Place the bolt in the topology blueprint
-    builder.setBolt("mergeData", bolt, taskCount)
-      .fieldsGrouping("truckEvents", new Fields("routeId"))
-      .fieldsGrouping("trafficData", new Fields("routeId"))
+    builder.setBolt("mergeData", bolt, taskCount).shuffleGrouping("truckData").shuffleGrouping("trafficData")
   }
 
   def buildNifiBolt()(implicit builder: TopologyBuilder): Unit = {
@@ -153,16 +135,10 @@ class TruckingTopology(config: TypeConfig) {
     val batchSize = config.getInt(TruckingTopology.NiFiInputBatchSize)
     val taskCount = config.getInt(Config.TOPOLOGY_TASKS)
 
-    val client = new SiteToSiteClient.Builder()
-      .url(nifiUrl)
-      .portName(nifiPortName)
-      .buildConfig()
+    // Construct a clientConfig and then the NiFi bolt
+    val clientConfig = new SiteToSiteClient.Builder().url(nifiUrl).portName(nifiPortName).buildConfig()
+    val nifiBolt = new NiFiBolt(clientConfig, new MergedDataPacketBuilder(), tickFrequency).withBatchSize(batchSize)
 
-    val packetBuilder = new TruckingPacketBuilder()
-    val nifiBolt = new NiFiBolt(client, packetBuilder, tickFrequency)
-      .withBatchSize(batchSize)
-
-    builder.setBolt("outToNifi", nifiBolt, taskCount)
-      .shuffleGrouping("mergeData")
+    builder.setBolt("toNifi", nifiBolt, taskCount).shuffleGrouping("mergeData")
   }
 }
